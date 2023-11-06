4a0c12d04d26e1540232c43e0626b9c2b7ad27df
Adrian Novegil <adrian.novegil@gmail.com>
2023-11-06 20:27:54 +0100

    Now we pass the matrix size and the block size as program argument

--- src/02/euclidean_vector_norm.cu
+++ src/02/euclidean_vector_norm.cu
@@ -16,54 +16,58 @@
 #include <stdio.h>
 
 // Values for MxN matrix
-#define M 100
-#define N 200
+const int DEFAULT_M = 100;
+const int DEFAULT_N = 200;
 
-const int block_size = 128; // CUDA maximum is 1024 *total* threads in block
+const int DEFAULT_BLOCK_SIZE = 128; // Default CUDA block size
 const float A_val = 1.0f;   // Default value for the all matrix elements
-const float C_res = 200.0f;   // Expected valur for the result vector
 
 // Kernel definition
 // ============================================================================
-__global__ void euclidean_vec_norm(float *a, float *c)
+__global__ void euclidean_vec_norm_GPU(float *a, float *c, int m, int n)
 {
     int row = blockDim.y * blockIdx.y + threadIdx.y;
 
-    if (row < M)
+    if (row < m)
     {
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < n; i++)
         {
-            c[row] += (a[row * N + i] * a[row * N + i]);
+            c[row] += (a[row * n + i] * a[row * n + i]);
         }
-        
     }
 }
 
 // Main program
 // ============================================================================
-int main()
+int main(int argc, char *argv[])
 {
+
+    // Read from args the matrix size
+    unsigned int m = (argc > 1) ? atoi(argv[1]) : DEFAULT_M;
+    unsigned int n = (argc > 2) ? atoi(argv[2]) : DEFAULT_N;
+    unsigned int block_size = (argc > 3) ? atoi(argv[3]) : DEFAULT_BLOCK_SIZE;
+
     // Number of bytes to allocate for N vector
-    size_t vectorNumBytes = M * sizeof(float);
+    size_t vectorNumBytes = m * sizeof(float);
     // Number of bytes to allocate for MxN matrix
-    size_t matrixNumBytes = M * N * sizeof(float);
+    size_t matrixNumBytes = m * n * sizeof(float);
 
     float *h_A, *h_C, *d_A, *d_C;
 
-    h_A = new float[M * N];
-    h_C = new float[M];
+    h_A = new float[m*n];
+    h_C = new float[m];
 
     // Allocate device memory and copy input data over to GPU
     cudaMalloc(&d_A, matrixNumBytes);
     cudaMalloc(&d_C, vectorNumBytes);
 
     // Initialize host matrix A
-    for (int i = 0; i < M * N; i++)
+    for (int i = 0; i < m*n; i++)
     {
         h_A[i] = A_val;
     }
     // Initialize host array C
-    for (int i = 0; i < M; i++)
+    for (int i = 0; i < m; i++)
     {
         h_C[i] = 0;
     }
@@ -80,18 +84,18 @@ int main()
                            block_size,
                            1); // dim3 variable holds 3 dimensions
     dim3 blocks_in_grid(1,
-                        ceil(float(M) / threads_per_block.y),
+                        ceil(float(m) / threads_per_block.y),
                         1);
-    euclidean_vec_norm<<<blocks_in_grid, threads_per_block>>>(d_A, d_C);
+    euclidean_vec_norm_GPU<<<blocks_in_grid, threads_per_block>>>(d_A, d_C, m, n);
 
     // Copy data from device to CPU
     cudaMemcpy(h_C, d_C, vectorNumBytes, cudaMemcpyDeviceToHost);
 
     // Verify results
-    for (int i = 0; i < M; i++)
-        if (h_C[i] != C_res)
+    for (int i = 0; i < m; i++)
+        if (h_C[i] != (float)n)
         {
-            printf("Mismatch at index %d, was: %f, should be: %f\n", i, h_C[i], C_res);
+            printf("Mismatch at index %d, was: %f, should be: %d\n", i, h_C[i], n);
             return -1;
         }
 
@@ -102,8 +106,8 @@ int main()
     printf("\n--------------------------------\n");
     printf("__SUCCESS__\n");
     printf("--------------------------------\n");
-    printf("M                         = %d\n", M);
-    printf("N                         = %d\n", N);
+    printf("M                         = %d\n", m);
+    printf("N                         = %d\n", n);
     printf("Threads Per Block (y-dim) = %d\n", threads_per_block.y);
     printf("Blocks In Grid (y-dim)    = %d\n", blocks_in_grid.y);
     printf("--------------------------------\n\n");
